420b47e5dfb97ebe5dc87ac7b565cdd5ba1c212b
Vishal Kandu <kanduvishal07@gmail.com>
2023-12-06 12:15:16 +0530

    Home page is created

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup"
+import Home from "./pages/home/Home"
 import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
