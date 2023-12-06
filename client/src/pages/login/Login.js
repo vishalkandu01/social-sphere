63a296314d7fce96a87207892341b146c10f6d76
Vishal Kandu <kanduvishal07@gmail.com>
2023-12-06 13:02:51 +0530

    axios setup is done

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -1,18 +1,43 @@
-import React from 'react';
-import './Login.scss';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Login.scss';
+import axiosClient from '../../utils/axiosClient';
 
 function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const result = await axiosClient.post('/auth/login', {
+            email,
+            password
+        });
+        console.log(result);
+    }
+
     return (
         <div className="Login">
             <div className="login-box">
                 <h2 className="heading">Login</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <label htmlFor="email">Eamil</label>
-                    <input type="email" className="email" id="email" />
+                    <input 
+                        type="email" 
+                        className="email" 
+                        id="email" 
+                        onChange={(e) => {setEmail(e.target.value)}} 
+                    />
 
                     <label htmlFor="password">Password</label>
-                    <input type="password" className='password' id='password' />
+                    <input 
+                        type="password" 
+                        className='password' 
+                        id='password' 
+                        onChange={(e)=>{setPassword(e.target.value)}}
+                    />
 
                     <input type="submit" className='submit' />
                 </form>
