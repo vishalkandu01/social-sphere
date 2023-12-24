05c546bcffea051ce579850b2291eac815907199
Vishal Kandu <kanduvishal07@gmail.com>
2023-12-24 18:53:35 +0530

    Follower is created

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Feed.scss'
 import Post from '../post/Post'
+import Follower from '../follower/Follower'
 
 function Feed() {
     return (
@@ -12,7 +13,24 @@ function Feed() {
                     <Post />
                     <Post />
                 </div>
-                <div className="right-part"></div>
+                <div className="right-part">
+                    <div className="following">
+                        <h3 className="title">You Are Following
+                        </h3>
+                        <Follower />
+                        <Follower />
+                        <Follower />
+                        <Follower />
+                    </div>
+                    <div className="suggestions">
+                        <h3 className="title">Suggested For You
+                        </h3>
+                        <Follower />
+                        <Follower />
+                        <Follower />
+                        <Follower />
+                    </div>
+                </div>
             </div>
         </div>
     )
