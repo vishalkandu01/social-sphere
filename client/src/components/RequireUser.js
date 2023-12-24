871a4efe5f6fde7e4cd0f1a8c604f2da1f5c0d74
Vishal Kandu <kanduvishal07@gmail.com>
2023-12-24 20:44:27 +0530

    updateProfile is created and everything is updated

--- client/src/components/RequireUser.js
+++ client/src/components/RequireUser.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { KEY_ACCESS_TOKEN, getItem } from '../utils/localStorageManager'
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router';
+import Login from '../pages/login/Login';
+import { getItem, KEY_ACCESS_TOKEN } from '../utils/localStorageManager'
 
 function RequireUser() {
     const user = getItem(KEY_ACCESS_TOKEN);
   return (
-    user ? <Outlet /> : <Navigate to="/login" />
+    user ? <Outlet /> : <Navigate to="/login"/>
   )
 }
 
--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -1,39 +1,38 @@
-import React from 'react'
-import './Feed.scss'
-import Post from '../post/Post'
+import React, { useEffect } from 'react'
 import Follower from '../follower/Follower'
+import Post from '../post/Post'
+import './Feed.scss'
+import {useSelector, useDispatch} from 'react-redux';
+import { getFeedData } from '../../redux/slices/feedSlice';
 
 function Feed() {
-    return (
-        <div className='Feed'>
-            <div className="container">
-                <div className="left-part">
-                    <Post />
-                    <Post />
-                    <Post />
-                    <Post />
-                </div>
-                <div className="right-part">
-                    <div className="following">
-                        <h3 className="title">You Are Following
-                        </h3>
-                        <Follower />
-                        <Follower />
-                        <Follower />
-                        <Follower />
-                    </div>
-                    <div className="suggestions">
-                        <h3 className="title">Suggested For You
-                        </h3>
-                        <Follower />
-                        <Follower />
-                        <Follower />
-                        <Follower />
-                    </div>
-                </div>
+
+  const dispatch = useDispatch();
+  const feedData = useSelector(state => state.feedDataReducer.feedData)
+
+  useEffect(() => {
+    dispatch(getFeedData());
+  }, [dispatch])
+
+  return (
+    <div className="Feed">
+        <div className="container">
+          <div className="left-part">
+            {feedData?.posts?.map(post => <Post key={post._id} post={post} />)}
+          </div>
+          <div className="right-part">
+            <div className="following">
+              <h3 className="title">You Are Following</h3>
+              {feedData?.followings?.map(user => <Follower key={user._id} user={user}/>)}
             </div>
+            <div className="suggestions">
+              <h3 className="title">Suggested For You</h3>
+              {feedData?.suggestions?.map(user => <Follower key={user._id} user={user}/>)}
+            </div>
+          </div>
         </div>
-    )
+    </div>
+  )
 }
 
 export default Feed
